69d504f5da5449f37c5e8c79af2b552ddf31bd4c
Pooja <studentpooja1234@gmail.com>
2024-04-12 10:54:56 +0530

    File Updated

---
+++ DSA_Topic_Wise/05_Stack/01_Leetcode/01_402_Remove_K_Digits/sol.cpp
@@ -0,0 +1,35 @@
+// Monotoniz stack- Stack in which we are trying to maintain particular order in the stack
+class Solution {
+public:
+   string removeKdigits(string num, int k) {
+        
+        string result = ""; //it will act like a stack
+        int n = num.length();
+        
+        for(int i = 0; i < n; i++) {
+            
+            while(result.length() > 0 && result.back() > num[i] && k > 0) {
+                result.pop_back();
+                k--;
+            }
+            
+            if(result.length() > 0 || num[i] != '0') {
+                result.push_back(num[i]); //to avoid the case when we have preceeding zeros
+            }
+            
+        }
+        
+        
+        while(result.length() > 0 && k > 0) {
+            result.pop_back();
+            k--;
+        }
+
+        if(result == "") {
+            return "0";
+        }
+        
+        return result;
+        
+    }
+};
